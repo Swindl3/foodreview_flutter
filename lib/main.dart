302754bf313b29474f81be56a759bd8332fef29a
PATTANAN <pattanan.la@rmuti.ac.th>
2019-03-09 23:54:10 +0700

    gygh

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 100;
+  int _counter = 600;
 
   void _incrementCounter() {
     setState(() { 
