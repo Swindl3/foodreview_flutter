5ed02db62bdb1b98b53229f59a2dd6617b9999c2
swindl3 <swindlemaxxx@gmail.com>
2019-03-11 01:48:31 +0700

    add new Feat

---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+class Config {
+  static String api_url = 'http://172.26.24.214:8080';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,166 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'config.dart';
+import 'dart:convert';
+import 'product.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    home: ECPApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ECPApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ECPApp();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 100;
-
-  void _incrementCounter() {
-    setState(() { 
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class _ECPApp extends State {
+  TextEditingController _userName = TextEditingController();
+  TextEditingController _passWord = TextEditingController();
+  void onLogin() {
+    Map<String, dynamic> param = Map();
+    param['userName'] = _userName.text;
+    param['passWord'] = _passWord.text;
+    http
+        .post('${Config.api_url}/userprofile/login', body: param)
+        .then((response) {
+      Map resMap = jsonDecode(response.body) as Map;
+      if (resMap['status'] == 0) {
+        int userId = resMap['data'];
+        print('user id ${userId}');
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (BuildContext context) => LandingScreen(userId)));
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('ECP'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          TextField(
+            controller: _userName,
+          ),
+          TextField(
+            controller: _passWord,
+          ),
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext) => RegisterScreen()));
+            },
+            child: Text('ลงทะเบียน'),
+          ),
+          RaisedButton(child: Text('ตกลง'), onPressed: onLogin)
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _RegisterScreen();
+  }
+}
+
+class _RegisterScreen extends State {
+  TextEditingController _username = TextEditingController();
+  TextEditingController _password1 = TextEditingController();
+  TextEditingController _password2 = TextEditingController();
+  TextEditingController _firstname = TextEditingController();
+  TextEditingController _lastname = TextEditingController();
+
+  bool _isError = false;
+  String _msg = null;
+  void onReg() {
+    if (_password1.text != _password2.text) {
+      _isError = true;
+      _msg = 'รหัสผ่านไม่ตรงกัน';
+    } else {
+      _isError = false;
+      Map<String, String> params = Map();
+      params['userName'] = _username.text;
+      params['passWord'] = _password1.text;
+      params['firstName'] = _firstname.text;
+      params['lastName'] = _lastname.text;
+      http
+          .post('${Config.api_url}/userprofile/registor', body: params)
+          .then((response) {
+        Map resMap = jsonDecode(response.body) as Map;
+        int status = resMap['status'];
+        if (status == 1) {
+          setState(() {
+            _isError = true;
+            _msg = resMap['message'];
+          });
+        } else {}
+      });
+    }
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ลงทะเบียน'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _isError
+              ? Text(
+                  _msg,
+                  style: TextStyle(color: Colors.red),
+                )
+              : Padding(
+                  padding: EdgeInsets.all(0.0),
+                ),
+          TextField(
+            controller: _username,
+            decoration: InputDecoration(hintText: 'UserName'),
+          ),
+          TextField(
+            controller: _password1,
+            decoration: InputDecoration(
+              hintText: 'รหัสผ่าน',
+            ),
+            obscureText: true,
+          ),
+          TextField(
+            controller: _password2,
+            decoration: InputDecoration(
+              hintText: 'รหัสผ่านอีกครั้ง',
+            ),
+            obscureText: true,
+          ),
+          TextField(
+            controller: _firstname,
+            decoration: InputDecoration(hintText: 'ชื่อ'),
+          ),
+          TextField(
+            controller: _lastname,
+            decoration: InputDecoration(hintText: 'นามสกุล'),
+          ),
+          RaisedButton(
+            onPressed: onReg,
+            child: Text('ตกลง'),
+          )
+        ],
+      ),
     );
   }
 }
